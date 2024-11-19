9fe1df586f66608d837b248cabc43d4ca5813f64
Jonas Almeida <jalmeida@mathbiol.org>
2024-11-19 10:54:46 -0500

    fully functional loadTCGAreports module
    reps = await (await import('https://epiverse.github.io/tcgapath/loadTCGAreports.mjs')).loadTCGAreports()

--- loadTCGAreports.mjs
+++ loadTCGAreports.mjs
@@ -1,34 +1,44 @@
 console.log(`import loadTCGAreports.mjs\n${Date()}`);
 
 const JSZip = (await import('https://esm.sh/jszip@3.10.1'))
+console.log(`0/4. Load key dependency, JSZip`)
 
 // const url = 'https://epiverse.github.io/tcgapath/TCGA_Reports.csv.zip' // default file url
 
 async function loadTCGAreports(url='https://epiverse.github.io/tcgapath/TCGA_Reports.csv.zip'){
-    const response = await fetch(url)
-    const data = response.arrayBuffer()
-    const zip = await JSZip.loadAsync(data);
-    let res = {}
-    for (let filename in zip.files) {
-            const file = zip.file(filename);
-            if (file) {
-                const content = await file.async('string');
-                // Do something with the content (e.g., display, process)
-                res[filename]=content
-                //console.log(filename, 'length:', content.length); 
-            }
-        }
-    res = (res['TCGA_Reports.csv']).split('\n').slice(1,-1) // remove first line with the headers "patient_filename,text"
-                                                            // and and trailing blank. Recall fence posting pitfall
-    res=res.map(function(row,i){
-        const id = row.match(/(^[^\,]+)/)[0]
-        const txt = row.slice(id.length+1)
+    // 1. load pathology reports
+    console.log(`1/4. Load pathology reports from ${url}`)
+    let response = await fetch(url)
+    let data = response.arrayBuffer()
+    let zip = await JSZip.loadAsync(data);
+    let filename='TCGA_Reports.csv'
+    let file = zip.file(filename)
+    let content = await file.async('string')
+    content = content.split('\n').slice(1,-1) // cut column headers and blank tail
+    // 2. parse ids and text for individual reports
+    console.log(`2/4. Parse individual reports into a object array structure`)
+    let reps=content.map(function(row,i){ // map individual reports
+        let id=row.match(/(^[^\,]+)/)[0] // patient id
         return {
+            i:i,
             id:id,
-            text:txt.slice(1,-1) // removing non standard start and end whitespace
+            text:row.slice(id.length+1).slice(1,-1) // removing non standard start and end whitespace
         }
     })
-    return res
+    console.log(`3/4. Load and parse individual embeddings ... wait for 4/4 ...`)
+    // 3. load embeddings reports
+    // note variable names being reused, no need to re-decale them
+    response = await fetch('https://epiverse.github.io/tcgapath/embeddings.tsv.zip')
+    data = response.arrayBuffer()
+    zip = await JSZip.loadAsync(data)
+    file = zip.file('embeddings.tsv')
+    content = await file.async('string')
+    content = content.split('\n')
+    content.forEach(function(row,i){
+        reps[i].embeddings=row.split('\t').map(JSON.parse)
+    })
+    console.log(`4/4. ... Done, wrapping up report structure`)
+    return reps
 }
 
 export{
