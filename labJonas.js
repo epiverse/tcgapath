b00a821e863794e6050335ee2a1793a41672fce7
Jonas Almeida <jalmeida@mathbiol.org>
2024-11-19 14:14:14 -0500

    html button to read data from sources

---
+++ labJonas.js
@@ -0,0 +1,17 @@
+console.log(`labJonas.js loaded\n${Date()}`);
+
+(async function(){
+   loadData = document.getElementById('loadData')
+   messages = document.getElementById('messages')
+   reps={}
+   const tcgaPath = await import('https://epiverse.github.io/tcgapath/loadTCGAreports.mjs')
+   loadData.onclick=async function(){
+       messages.innerHTML=' please wait, it typically takes a minute'
+       loadData.innerHTML='<span style="color:red;background-color:yellow">Loading ...</span>'
+       reps = await tcgaPath.loadTCGAreports()
+       console.log('reports',reps)
+       messages.innerHTML=`loaded: ${reps.length} reports embedded with ${reps[0].embeddings.length} dimensions`
+       loadData.innerHTML='<span style="color:blue;background-color:white">... Data loaded</span>'
+   }
+
+})()
