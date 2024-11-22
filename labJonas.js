808f1adb64ac715b7486b9d8f3f751547b865361
Jonas Almeida <jalmeida@mathbiol.org>
2024-11-22 13:39:40 -0500

    cleaning lab book

--- labJonas.js
+++ labJonas.js
@@ -16,12 +16,16 @@ console.log(`labJonas.js loaded\n${Date()}`);
        console.log('reports',reps)
        messages.innerHTML=`loaded: ${reps.length} reports embedded with ${reps[0].embeddings.length} dimensions`
        loadData.innerHTML='<span style="color:blue;background-color:white">... Data loaded</span>'
+       // activate dowload buttons
+       fullDownload.disabled=false
+       fullDownload.style.color='green'
    }
     fullDownload.onclick = async function(){
         if(Object.entries(reps).length==0){
             reps = await tcgaPath.loadTCGAreports()
         }
         tcgaPath.saveFullDataJSON(reps)
+        fullDownload.onmouseover=function(ev){ev.target.style.backgroundColor='yellow'}
     }
 
 
